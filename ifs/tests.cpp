49fa14c8db4006af7cbf0d18e0f395e667c88740
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-12-08 09:55:41 +0100

    constexpr if - exercise

--- ifs/tests.cpp
+++ ifs/tests.cpp
@@ -6,6 +6,7 @@
 #include <queue>
 #include <string>
 #include <vector>
+#include <string_view>
 
 #include "catch.hpp"
 
@@ -185,5 +186,97 @@ TEST_CASE("to_str")
     SECTION("c-string")
     {
         REQUIRE(to_str("text") == "text"s);
+    }
+}
+
+//////////////////////////////////////////////////
+// if constexpr + variadic templates
+
+namespace BeforeCpp17
+{
+    void print()
+    {
+        std::cout << "\n";
+    }
+
+    template <typename Head, typename... Tail>
+    void print(const Head& head, const Tail&... tail)
+    {
+        std::cout << head << " ";
+        print(tail...);
+    }
+}
+
+namespace SinceCpp17
+{
+    template <typename Head, typename... Tail>
+    void print(const Head& head, const Tail&... tail)
+    {
+        std::cout << head << " ";
+        
+        if constexpr(sizeof...(tail) > 0)
+        {
+            print(tail...);
+        }
+        else
+        {
+            std::cout << "\n";
+        }        
+    }
+}
+
+TEST_CASE("if constexpr + variadic templates")
+{
+    using namespace SinceCpp17;
+
+    print(1, 3.14, "text"s);
+    // print(head = 1, tail = (3.14, "text"s)...);
+    //    -> print(head = 3.14, tail = ("text"s)...)
+    //        -> print(head = "text"s, tail = ()...)
+    //            -> print()
+
+    print(1, 3.14, "text"s, "abc"sv);
+}
+
+template <typename TSource, typename TTarget, size_t NSource, size_t NTarget>
+void my_copy(TSource(&source)[NSource], TTarget(&target)[NTarget])
+{
+    static_assert(NTarget >= NSource, "size of target array must be larger than size of source array");
+
+    // T1 == T2 && T1 is trivially copyable
+    if constexpr(std::is_same_v<TSource, TTarget> && std::is_trivially_copyable_v<TSource>)
+    {
+        memcpy(target, source, NSource * sizeof(TSource));
+        std::cout << "copy by memcpy...\n";
+    }
+    else
+    {
+        for(size_t i = 0; i < NSource; ++i)
+            target[i] = source[i];
+        std::cout << "copy using loop...\n";
+    }
+}
+
+
+TEST_CASE("mcopy")
+{
+    SECTION("copy using loop")
+    {
+        std::string words1[] = {"one", "two", "three"};
+        std::string_view words2[3] = {};
+
+        my_copy(words1, words2);
+
+        REQUIRE(std::equal(begin(words2), end(words2), begin(words1)));
+    }
+
+    SECTION("int")
+    {
+        int tab1[] = {1, 2, 3};
+        int tab2[3];
+
+        my_copy(tab1, tab2);
+
+        REQUIRE(std::equal(begin(tab1), end(tab1), begin(tab2)));
     }
 }
