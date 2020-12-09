69fb2ef4fbbd008414454e35c7c49f34c01ae0cc
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-12-09 13:47:28 +0100

    string_view

--- _exercise/05_string_view/string_view_ex.cpp
+++ _exercise/05_string_view/string_view_ex.cpp
@@ -9,6 +9,32 @@
 
 using namespace std;
 
+std::vector<std::string_view> split_text(std::string&& text, string_view separators = " ,;") = delete;
+
+std::vector<std::string_view> split_text(string_view text, string_view separators = " ,;" )
+{
+    std::vector<std::string_view> tokens;   
+
+    auto pos1 = cbegin(text);
+
+    while(pos1 != cend(text))
+    {
+        auto pos2 = std::find_first_of(pos1, cend(text), cbegin(separators), cend(separators));
+
+        auto token_starts = &(*pos1);
+        auto token_length = pos2 - pos1;
+
+        tokens.emplace_back(token_starts, token_length);
+
+        if (pos2 == cend(text))
+            break;
+        
+        pos1 = std::next(pos2);
+    }
+
+    return tokens;
+}
+
 TEST_CASE("split with spaces")
 {
     string text = "one two three four";
